6dc777edb5db35c479d705904339a1e98c666b2f
Dan Kiss <flowfast47@gmail.com>
2019-12-01 18:24:10 -0800

    add link to code on github

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ export default () => {
     <div>
       <h1 className="display-4">About Contact Manager</h1>
       <p className="lead">Simple app to manage contacts</p>
-      <p className="text-secondary">Version 1.0.0</p>
+      <p className="text-secondary">Version 1.0.0 <a href="https://github.com/dan-kiss-dev-this/contact_manager_react_redux">See the code here</a></p>
     </div>
   );
 };

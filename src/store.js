fc7eda3985adde974ad6d93b3c97bd2965d61a05
Dan Kiss <flowfast47@gmail.com>
2019-12-01 22:31:08 -0800

    remove devtools

--- src/store.js
+++ src/store.js
@@ -10,6 +10,13 @@ const initialState = { contact: { contacts: [{ name: 'Rick' }] } };
 const middleware = [thunk];
 
 //as we create a store below which is is passed into the app we can call the dispatch function anywhere in the app as dispatch is a function of the redux store which is avalible though out the app
-const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
